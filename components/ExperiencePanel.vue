84e58d1b0c1bde7144e668686cd2606770d1c7a6
anthony <anthony.dionise@gmail.com>
2019-02-15 00:34:54 -0500

    better experience secitn

--- components/ExperiencePanel.vue
+++ components/ExperiencePanel.vue
@@ -22,12 +22,15 @@
         <v-card>
           <v-card-text class="pa-2">
             <v-alert value="true" color="grey darken-4">
-              <div>{{ item.desc }}</div>
-              <br>
-              <div>
-                <span v-if="item.stack" class="light-blue--text">
-                  Stack:
-                </span> {{ item.stack }}
+              <div v-for="(task, j) in item.tasks" :key="j">
+                <v-divider v-if="j > 0" class="my-3" />
+                <div>{{ task.desc }}</div>
+                <br>
+                <div>
+                  <span v-if="task.stack" class="light-blue--text">
+                    Stack:
+                  </span> {{ task.stack }}
+                </div>
               </div>
             </v-alert>
           </v-card-text>
@@ -45,29 +48,62 @@ export default {
         {
           company: 'Facility for Rare IsotopeBeams (FRIB)',
           title: 'Junior Software Developer - Controls Team',
-          desc:
-            'Developed a web application used to manage and track the installation of accelerator devices or components. This application allows technical experts to ensure that each device is ready for operation.',
-          stack: 'Node, Express, MongoDB, KnockoutJS, VuejS.'
+          tasks: [
+            {
+              desc:
+                'Developed a web application used to manage and track the installation of accelerator devices or components. This application allows technical experts to ensure that each device is ready for operation.',
+              stack: 'Node, Express, MongoDB, KnockoutJS, VuejS.'
+            }
+          ]
         },
         {
           company: 'Self Employed',
           title: 'Freelance eCommerce Developer',
-          desc:
-            'Developed eCommerce stores, landing pages, and advertising funnels.',
-          stack: 'Shopify, LimeLight, UltraCart, Ruby, Javascript, jQuery.'
+          tasks: [
+            {
+              desc:
+                'Developed eCommerce stores, landing pages, and advertising funnels.',
+              stack: 'Shopify, LimeLight, UltraCart, Ruby, Javascript, jQuery.'
+            }
+          ]
         },
         {
           company: 'Labor Law Poster Service',
           title: 'Web and Software Developer',
-          desc:
-            'Developed an adminstration system to handle multiple tasks for the employees. Such tasks include: routing incoming calls to available employees, printing and sending invoices, recording orders, etc.',
-          stack: 'CakePHP, Apache, Microsoft SQL Server'
+          tasks: [
+            {
+              desc:
+                'Developed an adminstration system to handle multiple tasks for the employees. Such tasks include: routing incoming calls to available employees, printing and sending invoices, recording orders, etc.',
+              stack: 'CakePHP, Apache, Microsoft SQL Server'
+            },
+            {
+              desc:
+                'Heavily influenced the projects structure by designing the new administration role functionality and database schema.'
+            },
+            {
+              desc: 'Worked on an Instagram influencer system',
+              stack: 'Ruby on the Rails'
+            },
+            {
+              desc: 'Fixed bugs in the legacy adminstration system',
+              stack: 'Cold Fusion'
+            }
+          ]
         },
         {
           company: 'National Superconducting Cyclotron Laboratory (NSCL)',
           title: 'Student Labor Aassistant',
-          desc: 'Developed a website front end for Control System Studio.',
-          stack: 'HTML, CSS, Javascript, jQuery, AJAX, Github API'
+          tasks: [
+            {
+              desc: 'Developed a website front end for Control System Studio.',
+              stack: 'HTML, CSS, Javascript, jQuery, AJAX, Github API'
+            },
+            {
+              desc:
+                'Worked on the UI of the Olog service with jQuery, a log book used by developers at NSCL.',
+              stack: 'jQuery'
+            }
+          ]
         }
       ]
     }
